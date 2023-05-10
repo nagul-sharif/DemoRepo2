dabd71db2820b711f040fdbea3fd2f8309d5d524
sharifarna <sharifarna@gmail.com>
2023-05-10 18:41:39 +0530

    This is first commit maded by sharif0705

---
+++ src/test/DemoClass2.java
@@ -0,0 +1,11 @@
+package test;
+
+public class DemoClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This line added by sharif0705");
+	}
+
+}
